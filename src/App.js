e6d3b5654243bca0261ac72d44e7dd85c5f90afa
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2021-03-02 20:52:11 +0200

    finally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Navbar />
+                <Navbar/>
                 <div className="content">
                     <Switch>
                         <Route exact path="/">
-                            <Home />
+                            <Home/>
                         </Route>
                         <Route path="/create">
-                            <Create />
+                            <Create/>
                         </Route>
                         <Route path="/blogs/:id">
-                            <BlogDetails />
+                            <BlogDetails/>
+                        </Route>
+                        <Route path="*">
+                            <NotFound/>
                         </Route>
                     </Switch>
                 </div>
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            // history.go(-1);
+            history.push('/');
+        })
+    }
+
     return (
         <div className="create">
-            <h2>New Blog</h2>
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Nick">Nick</option>
+                    <option value="Roma">Roma</option>
+                    <option value="Vova">Vova</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
